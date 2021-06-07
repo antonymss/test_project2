5bc2f4c6957c46c2b45771157d8f0c87b39542bf
antonymss <demeshkevichanton@gmail.com>
2021-06-07 20:08:51 +0300

    refact

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import {useFormik} from "formik";
 import style from "./Login.module.css"
-export const Login = ()=>{
+import {useDispatch, useSelector} from "react-redux";
+import {loginTC} from "./auth-reducer";
+import {Redirect} from "react-router-dom";
+import {AppRootStateType} from "../api/store";
 
+
+export const Login = () => {
+    const dispatch = useDispatch()
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
 
     const formik = useFormik({
         validate: (values) => {
@@ -18,18 +25,25 @@ export const Login = ()=>{
             }
         },
         initialValues: {
-            email: '',
-            password: '',
+            email: 'user@ozitag.com',
+            password: 'user',
             rememberMe: false
         },
+
         onSubmit: values => {
-            // dispatch(loginTC(values))
+
+            dispatch(loginTC(values));
+
         },
     })
+    if (isLoggedIn) {
+        return <Redirect to={'/'}/>
+    }
 
     return <div>
-        <form onSubmit={formik.handleSubmit}>
 
+        <form onSubmit={formik.handleSubmit}>
+            <div className={style.login}>
             <label>Email</label>
             <input type="email" {...formik.getFieldProps("email")}/>
             {formik.errors.email ? <div className={style.error}>{formik.errors.email}</div> : null}
@@ -38,10 +52,11 @@ export const Login = ()=>{
             <input type="password" {...formik.getFieldProps("password")}/>
             {formik.errors.password ? <div className={style.error}>{formik.errors.password}</div> : null}
 
-            <label>Remember Me<input type="checkbox" {...formik.getFieldProps("rememberMe")}
-                                     checked={formik.values.rememberMe}/></label>
+            {/*<label>Remember Me<input type="checkbox" {...formik.getFieldProps("rememberMe")}*/}
+            {/*                         checked={formik.values.rememberMe}/></label>*/}
 
             <button type={'submit'} color={'primary'}>Login</button>
+            </div>
         </form>
     </div>
 }
--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { LoginDataType } from "../api/api";
+import { AppRootStateType } from "../api/store";
+import { InitialAuthStateType, loginTC, logoutTC } from "../Login/auth-reducer";
+import style from "./Profile.module.css"
 
 export const Profile = ()=>{
-    return <div>
-        222
+
+    const {email,name} = useSelector<AppRootStateType, InitialAuthStateType>(state=>{
+        return state.auth
+    } )
+    const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn)
+   
+    const dispatch = useDispatch()
+
+    const onLogoutClick = () => {
+        dispatch(logoutTC())
+    }
+
+    if (!isLoggedIn) {
+        return <Redirect to={'/login'}/>
+    }
+
+    return <div className={style.login}>
+        <label>Name:</label>
+        {name}
+        <label>Email:</label>
+        {email}
+        <button onClick={onLogoutClick} >Log out</button>
     </div>
 }
