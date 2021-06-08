a1386f6b5566c01c2bd56faa87ca3f540b822dda
antonymss <demeshkevichanton@gmail.com>
2021-06-08 13:42:15 +0300

    refact

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,9 +1,10 @@
+import {Button, TextField } from "@material-ui/core";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { LoginDataType } from "../api/api";
 import { AppRootStateType } from "../api/store";
-import { InitialAuthStateType, loginTC, logoutTC } from "../Login/auth-reducer";
+import {getProfileTC, InitialAuthStateType, loginTC, logoutTC } from "../Login/auth-reducer";
 import style from "./Profile.module.css"
 
 export const Profile = ()=>{
@@ -19,15 +20,29 @@ export const Profile = ()=>{
         dispatch(logoutTC())
     }
 
+    // const getProfile = () => dispatch(getProfileTC())
+
     if (!isLoggedIn) {
         return <Redirect to={'/login'}/>
     }
 
-    return <div className={style.login}>
-        <label>Name:</label>
-        {name}
-        <label>Email:</label>
-        {email}
-        <button onClick={onLogoutClick} >Log out</button>
+    return <div className={style.profile}>
+        {/*<button onClick={getProfile}>refresh</button>*/}
+
+        <TextField
+            label="Name"
+            defaultValue={name}
+            variant="outlined"
+            disabled
+        />
+        <TextField
+            label="Email"
+            defaultValue={email}
+            variant="outlined"
+            disabled
+        />
+        <Button  onClick={onLogoutClick}  variant="contained" color="primary">
+            Log out
+        </Button>
     </div>
 }
